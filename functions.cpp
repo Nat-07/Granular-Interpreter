d8e0a3cbe61200c8ca57bf5b79adab613ca97802
Justin Morris <jnatmorris@gmail.com>
2021-06-07 02:14:49 -1000

    Version 0.2
    +New functions added (clearing terminal, checking syntax for + and - ), checking version, and some other improvements

--- functions.cpp
+++ functions.cpp
@@ -1,10 +1,135 @@
 // functions cpp file for granular
 #include <iostream>
 #include <cstdlib>
+#include <cstring>
 #include <string>
 using namespace std; 
 
-// print function
+// prototyped functions
+void print(string wordToPrint);
+void print(string wordToPrint, string value);
+void ver(char circumstance);
+bool properSyntaxCheck(string value, char operation);
+bool sum(string value);
+bool difference(string value);
+void clearTerminal();
+
+// ---------------------------------------------------------------------------
+
+// overloaded print functions
 void print(string wordToPrint){
     cout << wordToPrint << endl;
+}
+
+void print(string wordToPrint, string value){
+    cout << wordToPrint << value << endl;
+}
+
+// ---------------------------------------------------------------------------
+// helper function
+
+void help(){
+    print("____________________________________________");
+    ver('h');
+    print("Able commands:");
+    print("'quit' or 'clear'    Quit out of the Granular environment");
+    print("'sum'    Calculate the summation (addition) of numbers (e.g. 'sum 38+65+1' or 'sum 38 65 1' will output 104)");
+    print("____________________________________________\n");
+}
+
+// ---------------------------------------------------------------------------
+
+void clearTerminal(){
+    system("clear");
+    ver('c');
+    print("Type 'help' for able commands\n");
+}
+
+// ---------------------------------------------------------------------------
+
+void ver(char circumstance){
+
+    string version = "v0.2";
+
+    switch(circumstance){
+    case 'h':
+        print("Version: ", version);
+        break;
+    
+    case 'c':
+        print("Granular version ", version);
+        break;
+    
+    default:
+        print("Current version: ", version);
+        break;
+    }
+}
+
+// ---------------------------------------------------------------------------
+// helper function
+// checks for proper syntax with mathmatical operations
+bool properSyntaxCheck(string value, char operation){
+
+    for(int i = 0; i < value.size(); i++){
+        if(!isdigit(value[i]) && value[i] != operation && value[i] != ' ' && value[i] != '.' && value[i] != 13){
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+// ---------------------------------------------------------------------------
+
+bool sum(string value){
+
+    if(properSyntaxCheck(value, '+') == true){
+        return true;
+    }
+
+
+    long double sumVal = 0;
+    int start = 0;
+    
+
+    for(int i = 0; i < value.size(); i++){
+        
+        if(value[i] == '+' || value[i] == ' ' || value[i+1] == '\0'){
+
+            sumVal += atoi(value.substr(start, i).c_str());
+
+            start = i;
+
+        }
+    }
+
+    print("sum: ", to_string(sumVal));
+
+    return false;
+}
+
+bool difference(string value){
+
+    long double diffVal = 0;
+    int start = 0;
+
+    if(properSyntaxCheck(value, '-') == true){
+        return true;
+    }
+
+   for(int i = 0; i < value.size(); i++){
+        
+        if(value[i] == '-' || value[i] == ' ' || value[i+1] == '\0'){
+
+            diffVal += atoi(value.substr(start, i).c_str());
+
+            start = i;
+
+        }
+    }
+
+    print("difference: ", to_string(diffVal));
+
+    return false;
 }
--- main.cpp
+++ main.cpp
@@ -4,34 +4,56 @@
 #include "functions.cpp"
 using namespace std; 
 
-
 int main(){
     bool invalid = false;
+    //bool invalidNum = false;
     string userInput;
+
+    cout  << "Loading into Granular v0.1" << endl;
+    cout << "Type 'help' for able commands" << endl;
+    
 
     while(1){
         if(invalid == true){
            print("Invalid Syntax!");
         }
-        
+
         invalid = true;
         
         // get user input
         getline(cin, userInput);
 
-        if(userInput == "quit" || userInput == "clear"){
+        if(userInput == "quit"){ // quit function
             exit(0);
-        }
         
-        // print function
-        if(userInput.substr(0,5) == "print"){
+        }else if(userInput == "clear"){
+            clearTerminal();
+            invalid = false;
+
+        }else if(userInput.substr(0,4) == "help" || userInput.substr(0,1) == "?"){ // help function
+            help();
+            invalid = false;
+        
+        }else if(userInput.substr(0,3) == "ver" || userInput.substr(0,7) == "version"){ // version function
+            ver('h');
+            invalid = false;
+
+        }else if(userInput.substr(0,5) == "print"){ // print function
             print(userInput.substr(6));
             invalid = false;
+
+        }else if(userInput.substr(0,3) == "sum"){ // sum function
+            if(sum(userInput.substr(4)) == false){
+                invalid = false;
+            }
+            
+        }else if(userInput.substr(0,4) == "diff"){ // sum function
+            if(difference(userInput.substr(5)) == false){
+                invalid = false;
+            }
         }
-
-
-    }
     
+    }
 
     return 0;
 }
